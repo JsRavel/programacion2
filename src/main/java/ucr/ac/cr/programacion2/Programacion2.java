1ae473a0417c81b92fbab3efe45cf533c71f5fc3
Core i5 <jordystevenramirezesquivel@gmail.com>
2024-03-15 08:40:39 -0600

    just change the sisout string

--- src/main/java/ucr/ac/cr/programacion2/Programacion2.java
+++ src/main/java/ucr/ac/cr/programacion2/Programacion2.java
@@ -11,6 +11,6 @@ package ucr.ac.cr.programacion2;
 public class Programacion2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
