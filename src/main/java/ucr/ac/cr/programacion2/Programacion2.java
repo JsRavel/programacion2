a92bda983ef78d430e089ae957681ae732df4a71
Core i5 <jordystevenramirezesquivel@gmail.com>
2024-03-15 08:36:10 -0600

    firsts files

---
+++ src/main/java/ucr/ac/cr/programacion2/Programacion2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.programacion2;
+
+/**
+ *
+ * @author Core i5
+ */
+public class Programacion2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
